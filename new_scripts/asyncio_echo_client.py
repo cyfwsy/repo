9c269adcf28ecb42315415fc4835bf0314722837
cyfwsy <86754235+cyfwsy@users.noreply.github.com>
2022-05-31 14:31:30 +0800

    first commit
    modification

---
+++ new_scripts/asyncio_echo_client.py
@@ -0,0 +1,18 @@
+'''asyncio_client'''
+import asyncio
+
+async def tcp_echo_client(message):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+
+    print(f'Send: {message!r}')
+    writer.write(message.encode())
+    await writer.drain()
+
+    data = await reader.read(100)
+    print(f'Received: {data.decode()!r}')
+
+    print('Close the connection')
+    writer.close()
+
+asyncio.run(tcp_echo_client('Hello World!'))
