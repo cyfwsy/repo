ad9a8da41889828b8807c316d443b28e80459f61
cyfwsy <86754235+cyfwsy@users.noreply.github.com>
2022-06-01 16:12:16 +0800

    Update asyncio_echo_client.py
    a little change

--- new_scripts/asyncio_echo_client.py
+++ new_scripts/asyncio_echo_client.py
@@ -4,7 +4,7 @@ import asyncio
 async def tcp_echo_client(message):
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8888)
-
+    print("-----------------")
     print(f'Send: {message!r}')
     writer.write(message.encode())
     await writer.drain()
