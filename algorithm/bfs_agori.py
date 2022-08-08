eaf249b07f9a5299852764df45f0b20384887dea
west_laker <390879565@qq.com>
2022-08-08 14:46:42 +0800

    add  contents

--- algorithm/bfs_agori.py
+++ algorithm/bfs_agori.py
@@ -8,6 +8,7 @@ def bfs(graph,start=0):
     to_visit.appendleft(start)
     while to_visit:
         node = to_visit.pop()
+        print('----->>>  {}'.format(node))
         for neighbor in graph[node]:
             if dist[neighbor] == float('inf'):
                 dist[neighbor] = dist[node] + 1
@@ -15,8 +16,26 @@ def bfs(graph,start=0):
                 to_visit.appendleft(neighbor)
     return dist,prec
 
+def prec_node_map(prec,start):
+    "create a mapping node to post_node list"
+    map_node = {}
+    for i in range(len(prec)):
+        map_node[i] = []
+    for i in prec:
+        if i == None:
+            map_node['None'] = [start]
+        else:
+            for j in range(len(prec)):
+                if prec[j] == i:
+                    map_node[int(i)].append(j)
+    for i in map_node.keys():
+        map_node[i] = set(map_node[i])
+    return map_node
+    
+            
 graph_1 = [[1,4],[0,5],[3,5],[2,6],[0,5,6],[1,2,4],[3,4]]
-dist,prec = bfs(graph_1,start=6)
+dist,prec = bfs(graph_1,start=5)
 print(dist)
 print(prec)
+print(prec_node_map(prec,5))
     
