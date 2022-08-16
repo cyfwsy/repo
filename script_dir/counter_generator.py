e55f3ebd4270c8bdc2126299a985c7e2ffc89af1
west_laker <390879565@qq.com>
2022-08-16 16:33:18 +0800

    initial commit

---
+++ script_dir/counter_generator.py
@@ -0,0 +1,18 @@
+def counter(maximum):
+    i = 0
+    while i < maximum:
+        val = (yield i)
+        # If value provided, change counter
+        if val is not None:
+            i = val
+        else:
+            i += 1
+            
+g = counter(10)
+print(g)
+print(next(g))
+print(next(g))
+print(g.send(7))
+print(next(g))
+print(next(g))
+print(g.__next__())
