462fba8cc009a38d52b6a3a1cd0ff39937617b70
west_laker <390879565@qq.com>
2022-08-26 09:31:11 +0800

    a little change

--- script_dir/async_co_producer_consumer.py
+++ script_dir/async_co_producer_consumer.py
@@ -5,8 +5,8 @@ async def consumer(n,q):
         print('consumer {}:waiting for item'.format(n))
         item = await q.get()
         print('consumer {}: has item {}'.format(n,item))
-        if item is None:
-            # None is the signal to stop.
+        if item is "STOP":
+            # 'stop' is the signal to stop.
             q.task_done()
             break
         else:
@@ -15,34 +15,34 @@ async def consumer(n,q):
         print('consumer {}: ending'.format(n))
 
 
-async def producer(q,num_workers):
+async def producer(q,num_consumers):
     print('producer: starting')
     #Add some numbers to the queue to simulate jobs.
-    for i in range(num_workers * 3):
+    for i in range(num_consumers * 3):
         await q.put(i)
-        print('producer: added task {} to the queue'.format(i))
+        print('producer: add task {} to the queue'.format(i))
     #Add None entries in the queue to signal the consumer to exit
-    print('produer: adding stop signal to the queue')
-    for i in range(num_workers):
-        await q.put(None)
+    print('producer: adding stop signal to the queue')
+    for i in range(num_consumers):
+        await q.put('STOP')
     print('producer: waiting for queue to empty')
     await q.join()
     print('producer ending')
 
-async def main(loop,num_workers):
+async def main(loop,num_consumers):
     #Create the queue with a fixed size so the producer will block until
     #the consumer pull some items out.
-    q = asyncio.Queue(maxsize=3)
+    q = asyncio.Queue(maxsize=2)
 
     #schedule the producer task
-    consumers = [loop.create_task(consumer(i,q)) for i in range(4)]
-    prod = loop.create_task(producer(q,num_workers))
+    consumers = [loop.create_task(consumer(i,q)) for i in range(num_consumers)]
+    prod = loop.create_task(producer(q,num_consumers))
     #wait for all of the coroutines to finish
     await asyncio.wait(consumers + [prod])
 
 event_loop = asyncio.get_event_loop()
 try:
-    event_loop.run_until_complete(main(event_loop,2))
+    event_loop.run_until_complete(main(event_loop,3))
 finally:
     event_loop.close()
 
